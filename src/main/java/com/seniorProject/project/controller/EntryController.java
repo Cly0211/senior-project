edb214eb8d0cab9269f93dc016d9007eb9ea4bce
Liyi <chenliyi0211@126.com>
2023-10-18 15:42:02 -0400

    entry add & update

--- src/main/java/com/seniorProject/project/controller/EntryController.java
+++ src/main/java/com/seniorProject/project/controller/EntryController.java
@@ -1,6 +1,7 @@
 package com.seniorProject.project.controller;
 
 import com.seniorProject.project.model.Entry;
+import com.seniorProject.project.model.User;
 import com.seniorProject.project.service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +18,27 @@ public class EntryController {
     @GetMapping("/getId/{id}")
     public List<Entry> getId(@PathVariable String id){
         try{
-            List<Entry> list = entryService.selectId(id);
-            //System.out.println(list.get(0).getId());
-            return list;
+            return entryService.selectId(id);
         }catch(Exception e){
             throw new RuntimeException("error when searching entries: "+id);
+        }
+    }
+
+    @PostMapping("/add")
+    public void addEntry(@RequestBody Entry entry){
+        try{
+            entryService.insertEntry(entry);
+        }catch(Exception e){
+            throw new RuntimeException("error when inserting entries: "+entry);
+        }
+    }
+
+    @PutMapping("/update")
+    public void updateUser(@RequestBody Entry entry){
+        try{
+            entryService.updateEntry(entry);
+        }catch(Exception e){
+            throw new RuntimeException("error when updating entry: "+entry);
         }
     }
 }
--- src/main/java/com/seniorProject/project/mapper/EntryMapper.java
+++ src/main/java/com/seniorProject/project/mapper/EntryMapper.java
@@ -1,8 +1,11 @@
 package com.seniorProject.project.mapper;
 
+import com.seniorProject.project.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import com.seniorProject.project.model.Entry;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -11,4 +14,10 @@ public interface EntryMapper {
 
     @Select("select * from `entries` where id = #{id}")
     List<Entry> selectEntry(String id);
+
+    @Insert("insert into `entries` (id, entryDate, mood, activities, journal) values (#{id},#{entryDate},#{mood},#{activities},#{journal})")
+    void insert(Entry entry);
+
+    @Update("update `entries` set mood = #{mood}, activities = #{activities}, journal = #{journal} where id = #{id} and entryDate = #{entryDate}")
+    void updateEntry(Entry entry);
 }
